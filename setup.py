71e7a7c2b83803924c74926ccfa720e62d3442f0
Iam54r1n4 <Iam54r1n4@users.noreply.github.com>
2023-11-12 07:36:55 +0000

    ✅ Ready to clone and code.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-"""Python setup.py for project_name package"""
+"""Python setup.py for hiddify_tg_bot package"""
 import io
 import os
 from setuptools import find_packages, setup
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 def read(*paths, **kwargs):
     """Read the contents of a text file safely.
-    >>> read("project_name", "VERSION")
+    >>> read("hiddify_tg_bot", "VERSION")
     '0.1.0'
     >>> read("README.md")
     ...
@@ -30,17 +30,17 @@ def read_requirements(path):
 
 
 setup(
-    name="project_name",
-    version=read("project_name", "VERSION"),
-    description="project_description",
-    url="https://github.com/author_name/project_urlname/",
+    name="hiddify_tg_bot",
+    version=read("hiddify_tg_bot", "VERSION"),
+    description="Awesome hiddify_tg_bot created by Iam54r1n4",
+    url="https://github.com/Iam54r1n4/hiddify-tg-bot/",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
-    author="author_name",
+    author="Iam54r1n4",
     packages=find_packages(exclude=["tests", ".github"]),
     install_requires=read_requirements("requirements.txt"),
     entry_points={
-        "console_scripts": ["project_name = project_name.__main__:main"]
+        "console_scripts": ["hiddify_tg_bot = hiddify_tg_bot.__main__:main"]
     },
     extras_require={"test": read_requirements("requirements-test.txt")},
 )
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
-from project_name.base import NAME
+from hiddify_tg_bot.base import NAME
 
 
 def test_base():
-    assert NAME == "project_name"
+    assert NAME == "hiddify_tg_bot"

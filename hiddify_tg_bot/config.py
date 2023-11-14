b45a3260cfd592ce37482f3bede95ca9f9dadfc8
Sarina <iam54r1n40@gmail.com>
2023-11-15 00:20:27 +0330

    update: config.py

--- hiddify_tg_bot/config.py
+++ hiddify_tg_bot/config.py
@@ -2,4 +2,5 @@ BOT_TOKEN = "<TOKEN>"
 # example: https://some.domain.com/
 HIDDIFY_ROOT_DOMAIN = "<ROOT_DOMAIN>"
 HIDDIFY_PROXY_PATH = "<PROXY_PATH>"
-LANG = "EN"
+REDIS_URI = "redis://127.0.0.1:6379"
+TRASLATION_FOLDER = "bot/translation/"

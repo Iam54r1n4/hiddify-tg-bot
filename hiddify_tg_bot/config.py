fa7e376381920be69ed81c9a09ac7d8095bf8691
Sarina <iam54r1n40@gmail.com>
2023-11-16 11:05:49 +0330

    update: config.py

--- hiddify_tg_bot/config.py
+++ hiddify_tg_bot/config.py
@@ -1,6 +1,7 @@
+import os
 BOT_TOKEN = "<TOKEN>"
 # example: https://some.domain.com/
 HIDDIFY_ROOT_DOMAIN = "<ROOT_DOMAIN>"
 HIDDIFY_PROXY_PATH = "<PROXY_PATH>"
 REDIS_URI = "redis://127.0.0.1:6379"
-TRASLATION_FOLDER = "bot/translation/"
+TRASLATION_FOLDER = f"{os.path.join(os.path.dirname(os.path.abspath(__file__)),'bot/translation/')}"
